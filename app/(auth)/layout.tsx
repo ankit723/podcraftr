990b287b82c36dfc46119a0afc07836ad35f2bbf
Ankit Biswas <varanasiartist.omg@gmail.com>
2024-06-12 01:20:19 +0530

    authentication done...please diverge from here if you want to implement self database and authentication...if the free plan exhausts

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,13 +1,16 @@
-import LeftSideBar from "@/components/shared/leftSideBar";
-import RightSideBar from "@/components/shared/rightSideBar";
+import Image from "next/image";
+
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
-    <main>
-        {children}
+    <main className="relative h-screen w-full">
+      <div className="absolute size-full">
+        <Image src="/images/bg-img.png" alt="background" fill className="size-full"/>
+      </div>
+      {children}
     </main>
   );
 }
