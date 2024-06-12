e32345a2650dcdf77aff262eaa70857c5b0cade1
Ankit Biswas <varanasiartist.omg@gmail.com>
2024-06-12 17:42:00 +0530

    test change

--- app/(root)/create-podcast/page.tsx
+++ app/(root)/create-podcast/page.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const CreatePodcast = () => {
   return (
     <div>
-      <h1 className='text-20 font-bold text-white-1'>Create Podcast</h1>
+      <h1 className='text-20 font-bold text-white-1'>Create Podcasegeeg4h4h4h4ht</h1>
 
     </div>
   )
