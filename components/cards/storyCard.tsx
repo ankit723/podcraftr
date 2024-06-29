3b464383dcad7e456e3ad0ba2fdc62f0abbec340
Ankit Biswas <varanasiartist.omg@gmail.com>
2024-06-29 19:54:02 +0530

    storyCard added

--- components/cards/storyCard.tsx
+++ components/cards/storyCard.tsx
@@ -4,12 +4,10 @@ const StoryCard = ({storyPrompt}:any) => {
   return (
     <div>
       {
-        storyPrompt.map((prompt:any)=>(
-          <>
-          <div className="my-2 border-b py-3 text-white-2">
+        storyPrompt.map((prompt:any, index:number)=>(
+          <div key={index} className="my-2 border-b border-black-4 py-3 text-white-2">
             <span className='text-white-1 '>{prompt.characterName}: </span> &nbsp; {prompt.speech}
           </div>
-          </>
         ))
       }
     </div>
